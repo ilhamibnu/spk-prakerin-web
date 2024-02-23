d57bb6bccc575c6c630942f982c7fb90575978fb
ilhamibnu <e41210178@student.polije.ac.id>
2024-02-24 01:35:54 +0700

    templating blade

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.pages.login');
+});
+
+Route::get('/register', function () {
+    return view('admin.pages.register');
+});
+
+Route::get('/dashboard', function () {
+    return view('admin.pages.dashboard');
 });
