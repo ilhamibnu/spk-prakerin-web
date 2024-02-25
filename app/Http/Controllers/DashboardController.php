66bf259fa230ca9cba995abd29ce388944747ae8
ilhamibnu <e41210178@student.polije.ac.id>
2024-02-25 12:44:08 +0700

    feat dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kriteria;
+use App\Models\Mitra;
 use Illuminate\Http\Request;
+use App\Models\Siswa;
+use App\Models\User;
+
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.pages.dashboard');
+        $siswa = Siswa::count();
+        $mitra = Mitra::count();
+        $user = User::where('id_role', '2')->count();
+        $kriteria = Kriteria::count();
+        return view('admin.pages.dashboard', [
+            'siswa' => $siswa,
+            'mitra' => $mitra,
+            'user' => $user,
+            'kriteria' => $kriteria
+        ]);
     }
 }
