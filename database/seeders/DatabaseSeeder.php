d6ac8f75450fc76d67698f67ac9aedc6fb165f06
ilhamibnu <e41210178@student.polije.ac.id>
2024-02-26 18:25:11 +0700

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Kriteria;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -31,6 +32,36 @@ class DatabaseSeeder extends Seeder
             'email' => 'user@gmail.com',
             'password' => bcrypt('user'),
             'id_role' => 2,
+        ]);
+
+        Kriteria::create([
+            'name' => 'Ftth',
+            'bobot' => 0.3,
+            'jenis' => 'Cost',
+        ]);
+
+        Kriteria::create([
+            'name' => 'Cpe',
+            'bobot' => 0.2,
+            'jenis' => 'Cost',
+        ]);
+
+        Kriteria::create([
+            'name' => 'Absensi',
+            'bobot' => 0.2,
+            'jenis' => 'Cost',
+        ]);
+
+        Kriteria::create([
+            'name' => 'Kjt',
+            'bobot' => 0.2,
+            'jenis' => 'Benefit',
+        ]);
+
+        Kriteria::create([
+            'name' => 'Nilai Semester',
+            'bobot' => 0.2,
+            'jenis' => 'Benefit',
         ]);
     }
 }
--- resources/views/admin/pages/data-kriteria.blade.php
+++ resources/views/admin/pages/data-kriteria.blade.php
@@ -13,9 +13,9 @@
                 <div class="card-header">
                     <h4 class="card-title">Data Kriteria</h4>
                 </div>
-                <div class="text-end m-2">
+                {{-- <div class="text-end m-2">
                     <a href="#" data-bs-toggle="modal" data-bs-target="#Add" class="btn btn-success shadow btn-xs sharp me-1"><i class="fa fa-add"></i></a>
-                </div>
+                </div> --}}
                 <div class="card-body">
                     @if($errors->any())
                     <div class="alert alert-danger alert-dismissible fade show mt-2">
@@ -54,7 +54,7 @@
                                     <td>
                                         <div class="d-flex">
                                             <a href="#" data-bs-toggle="modal" data-bs-target="#Edit{{ $data->id }}" class="btn btn-primary shadow btn-xs sharp me-1"><i class="fa fa-pencil"></i></a>
-                                            <a href="#" data-bs-toggle="modal" data-bs-target="#Delete{{ $data->id }}" class="btn btn-danger shadow btn-xs sharp"><i class="fa fa-trash"></i></a>
+                                            {{-- <a href="#" data-bs-toggle="modal" data-bs-target="#Delete{{ $data->id }}" class="btn btn-danger shadow btn-xs sharp"><i class="fa fa-trash"></i></a> --}}
                                         </div>
                                     </td>
                                 </tr>
