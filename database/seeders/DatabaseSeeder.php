fe9064850d58144d1cfed4a7abfd9cf74e3cee29
ilhamibnu <e41210178@student.polije.ac.id>
2024-03-05 12:17:52 +0700

    update seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,9 @@ use App\Models\Ftth;
 use App\Models\Kjt;
 use App\Models\NilaiSemester;
 use App\Models\Kriteria;
+use App\Models\Mitra;
 use App\Models\Role;
+use App\Models\Siswa;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -42,13 +44,13 @@ class DatabaseSeeder extends Seeder
         Kriteria::create([
             'name' => 'Ftth',
             'bobot' => 0.3,
-            'jenis' => 'Cost',
+            'jenis' => 'Benefit',
         ]);
 
         Kriteria::create([
             'name' => 'Cpe',
             'bobot' => 0.2,
-            'jenis' => 'Cost',
+            'jenis' => 'Benefit',
         ]);
 
         Kriteria::create([
@@ -66,7 +68,7 @@ class DatabaseSeeder extends Seeder
         Kriteria::create([
             'name' => 'Nilai Semester',
             'bobot' => 0.2,
-            'jenis' => 'Benefit',
+            'jenis' => 'Cost',
         ]);
 
         Absensi::create([
@@ -198,5 +200,109 @@ class DatabaseSeeder extends Seeder
             'bobot' => 1
         ]);
 
+        Siswa::create([
+            'nisn' => '123',
+            'name' => 'Siswa 1',
+            'id_ftth' => 2,
+            'id_cpe' => 4,
+            'id_absensi' => 3,
+            'id_kjt' => 3,
+            'id_nilai_semester' => 5,
+        ]);
+
+        Siswa::create([
+            'nisn' => '124',
+            'name' => 'Siswa 2',
+            'id_ftth' => 3,
+            'id_cpe' => 5,
+            'id_absensi' => 4,
+            'id_kjt' => 4,
+            'id_nilai_semester' => 4,
+        ]);
+
+        Siswa::create([
+            'nisn' => '125',
+            'name' => 'Siswa 3',
+            'id_ftth' => 4,
+            'id_cpe' => 3,
+            'id_absensi' => 5,
+            'id_kjt' => 5,
+            'id_nilai_semester' => 3,
+        ]);
+
+        Siswa::create([
+            'nisn' => '126',
+            'name' => 'Siswa 4',
+            'id_ftth' => 5,
+            'id_cpe' => 2,
+            'id_absensi' => 2,
+            'id_kjt' => 2,
+            'id_nilai_semester' => 2,
+        ]);
+
+        Siswa::create([
+            'nisn' => '127',
+            'name' => 'Siswa 5',
+            'id_ftth' => 1,
+            'id_cpe' => 1,
+            'id_absensi' => 1,
+            'id_kjt' => 1,
+            'id_nilai_semester' => 1,
+        ]);
+
+        Mitra::create([
+            'name' => 'Mitra 1',
+            'deskripsi' => 'Mitra 1 deskripsi',
+            'alamat' => 'Mitra 1 alamat',
+            'id_ftth' => 5,
+            'id_cpe' => 4,
+            'id_absensi' => 4,
+            'id_kjt' => 3,
+            'id_nilai_semester' => 2,
+        ]);
+
+        Mitra::create([
+            'name' => 'Mitra 2',
+            'deskripsi' => 'Mitra 2 deskripsi',
+            'alamat' => 'Mitra 2 alamat',
+            'id_ftth' => 4,
+            'id_cpe' => 3,
+            'id_absensi' => 3,
+            'id_kjt' => 2,
+            'id_nilai_semester' => 1,
+        ]);
+
+        Mitra::create([
+            'name' => 'Mitra 3',
+            'deskripsi' => 'Mitra 3 deskripsi',
+            'alamat' => 'Mitra 3 alamat',
+            'id_ftth' => 3,
+            'id_cpe' => 2,
+            'id_absensi' => 2,
+            'id_kjt' => 1,
+            'id_nilai_semester' => 5,
+        ]);
+
+        Mitra::create([
+            'name' => 'Mitra 4',
+            'deskripsi' => 'Mitra 4 deskripsi',
+            'alamat' => 'Mitra 4 alamat',
+            'id_ftth' => 2,
+            'id_cpe' => 1,
+            'id_absensi' => 1,
+            'id_kjt' => 5,
+            'id_nilai_semester' => 4,
+        ]);
+
+        Mitra::create([
+            'name' => 'Mitra 5',
+            'deskripsi' => 'Mitra 5 deskripsi',
+            'alamat' => 'Mitra 5 alamat',
+            'id_ftth' => 1,
+            'id_cpe' => 5,
+            'id_absensi' => 5,
+            'id_kjt' => 4,
+            'id_nilai_semester' => 3,
+        ]);
     }
 }
